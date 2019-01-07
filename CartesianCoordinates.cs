31120ffc5d6cc3ea506d565a54bd006e1e8b5ccf
laugof <laugof@gmail.com>
2019-01-07 23:34:29 +0100

    Interfaces become generic <T>

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -4,50 +4,51 @@
 /// </summary>
 namespace Core.Interface
 {
+
     /// <summary>
     /// Vector of cartesian coordinates
     /// </summary>
-    public interface IVector
+    public interface IVector<T>
     {
-        double this[int index] { get; set; }
+        T this[int index] { get; set; }
         int Length { get; }
     }
 
     /// <summary>
     /// 2d cartesian coordinates
     /// </summary>
-    public interface I2dCartesianCoordinates : IVector
+    public interface I2dCartesianCoordinates<T> : IVector<T>
     {
-        double X { get; set; }
-        double Y { get; set; }
-        void SetCartesian(double x, double y);
-        void SetCartesian(I2dCartesianCoordinates p);
+        T X { get; set; }
+        T Y { get; set; }
+        void SetCartesian(T x, T y);
+        void SetCartesian(I2dCartesianCoordinates<T> p);
     }
 
     /// <summary>
     /// 3d cartesian coordinates
     /// </summary>
-    public interface I3dCartesianCoordinates : IVector
+    public interface I3dCartesianCoordinates<T> : IVector<T>
     {
-        double X { get; set; }
-        double Y { get; set; }
-        double Z { get; set; }
-        void SetCartesian(double x, double y, double z = 0.0);
-        void SetCartesian(I2dCartesianCoordinates p);
-        void SetCartesian(I3dCartesianCoordinates p);
+        T X { get; set; }
+        T Y { get; set; }
+        T Z { get; set; }
+        void SetCartesian(T x, T y, T z);
+        void SetCartesian(I2dCartesianCoordinates<T> p);
+        void SetCartesian(I3dCartesianCoordinates<T> p);
     }
 
     /// <summary>
     /// Distance with a 3d point
     /// </summary>
-    public interface I3dPointDistance
+    public interface I3dPointDistance<T>
     {
-        double Distance(I3dCartesianCoordinates p);
+        double Distance(I3dCartesianCoordinates<T> p);
     }
 
     /// <summary>
     /// 3d cartesian metric
     /// </summary>
-    public interface I3dCartesianMetric : I3dCartesianCoordinates, I3dPointDistance
+    public interface I3dCartesianMetric<T> : I3dCartesianCoordinates<T>, I3dPointDistance<T>
     { }
 }
