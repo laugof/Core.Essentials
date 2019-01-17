f208ccd8a145ff0eec04f0aa70b34b7ffb9ea51a
laugof <laugof@gmail.com>
2019-01-17 16:52:33 +0100

    New interface: IFrame

---
+++ EuclideanSpace.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Author: Laurent Goffin
+/// Generic interfaces
+/// </summary>
+namespace Core.Interface
+{
+    /// <summary>
+    /// Reference frame interface
+    /// </summary>
+    public interface IFrame
+    { }
+}
