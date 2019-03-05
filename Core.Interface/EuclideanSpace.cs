ef197d842c87bdbe724e80e7402721d485eaf27b
laugof <laugof@gmail.com>
2019-03-05 17:28:14 +0100

    New interface IEuclideanSpace

--- Core.Interface/EuclideanSpace.cs
+++ Core.Interface/EuclideanSpace.cs
@@ -7,6 +7,15 @@ namespace Core.Interface
     /// <summary>
     /// Reference frame interface
     /// </summary>
+    public interface IEuclideanSpace
+    {
+        bool Set(IFrame origin, IFrame target, IMatrix<double> rigidMatrix = null);
+        IMatrix<double> Get(IFrame origin, IFrame target);
+    }
+
+    /// <summary>
+    /// Reference frame interface
+    /// </summary>
     public interface IFrame
     { }
 }
