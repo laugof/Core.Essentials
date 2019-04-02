480c92b1516d27c106a181e0e5af4417311abe32
laugof <laugof@gmail.com>
2019-04-02 16:14:47 +0200

    New interface ITimestamped

---
+++ Core.Interface/Time.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Interface
+{
+    /// <summary>
+    /// Author : Laurent Goffin
+    /// Timestamped object
+    /// </summary>
+    public interface ITimestamped
+    {
+        System.DateTime DateTime { get; set; }
+    }
+}
